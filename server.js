142a5aece680f23be495722494d75d6d3bcc52da
Karen Ibarra <karenibarra@pop-os.localdomain>
2021-01-13 09:03:14 -0600

    added login

--- server.js
+++ server.js
@@ -2,16 +2,29 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const puppeteer = require('puppeteer');
+require('dotenv').config()
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
 
 app.get('/new', (req, res) => {
     (async () => {
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
-        await page.goto('https://www.seminuevos.com/');
-        await page.screenshot({path: 'images/example.png'});
-        await browser.close();
+        await page.goto('https://www.seminuevos.com/login');
+
+        await page.type('#email_login', email);
+        await page.type('#password_login', password);
+
+        await page.waitForSelector('button[type="submit"]', {
+          visible: true,
+        });
+
+        await page.click('button[type="submit"]');
+
+        await page.screenshot({path: 'client/public/images/example.png'});
       })();
   res.send({ express: 'App working' });
 });
